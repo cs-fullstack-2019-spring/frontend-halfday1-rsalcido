dbe229952f4388260a1eb90403f725121586f202
Ricardo Salcido <rsalcido@liberty.edu>
2019-02-11 16:52:36 -0600

    Week6

--- Week6CW.js
+++ Week6CW.js
@@ -1,8 +1,26 @@
+var btn1=document.getElementById("btn1");
 
-var button = document.getElementById("Player1Counter"),
-    count = 0;
-button.onclick = function() {
+var button = document.getElementById("Player1Counter");
+var count = 0;
+
+button.addEventListener("click", buttonClickFunction);
+
+function buttonClickFunction() {
+    console.log("Clicked button");
     count += 1;
-    button.innerHTML = "Click me: " + count;
+    btn1.innerText = "" + count;
 };
 
+var btn2=document.getElementById("btn2");
+
+var button2 = document.getElementById("Player2Counter");
+var count2 = 0;
+button2.addEventListener("click", buttonClickFunction2);
+
+function buttonClickFunction2() {
+    console.log("Clicked button");
+    count += 1;
+    btn2.innerText = "" + count;
+};
+
+$("#btn5").onclick(background, red);
